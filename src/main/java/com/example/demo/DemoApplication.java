1eefcf2256e1b5d950ec12c54f76461391f0cf97
Strokova Irina <i.strokova@rambler-co.ru>
2020-09-08 20:08:46 +0300

    -

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/index.html");
+    }
 }
